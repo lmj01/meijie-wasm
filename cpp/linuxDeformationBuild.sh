52d2d9bd2bc417c8e54762b11f2e9414fce4c66b
lmj01 <lmjie_good@163.com>
2024-12-26 17:19:09 +0800

    add deformation for wasm

---
+++ cpp/linuxDeformationBuild.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+SOURCE_DIR="$PWD/deformation"
+TOOLCHAIN="$PWD/build/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
+BUILD_DIR="$PWD/bdeformation"
+
+export EMSCRIPTEN="$PWD/build/emsdk/upstream/emscripten"
+
+if [ ! -d "${BUILD_DIR}" ]; then
+    mkdir -p "${BUILD_DIR}"
+    echo "fold not exist, and create new : $BUILD_DIR"
+else 
+    echo "fold exist: $BUILD_DIR"
+fi
+cd "${BUILD_DIR}"
+
+cmake "${SOURCE_DIR}" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN}" -DDRACO_WASM=ON
+make 
+echo "-- build complete --"
+
