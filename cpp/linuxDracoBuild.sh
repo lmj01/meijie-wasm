d7624cef41db3f26ceb29204137770287bd79d14
lmj01 <lmjie_good@163.com>
2024-12-26 16:55:46 +0800

    update

--- cpp/linuxDracoBuild.sh
+++ cpp/linuxDracoBuild.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-SOURCE_DIR="build/draco"
-BUILD_DIR="builddraco"
+SOURCE_DIR="$PWD/build/draco"
+TOOLCHAIN="$PWD/build/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
+BUILD_DIR="$PWD/builddraco"
+
+export EMSCRIPTEN="$PWD/build/emsdk/upstream/emscripten"
 
 if [ ! -d "${BUILD_DIR}" ]; then
     mkdir -p "${BUILD_DIR}"
@@ -8,9 +11,9 @@ if [ ! -d "${BUILD_DIR}" ]; then
 else 
     echo "fold exist: $BUILD_DIR"
 fi
-#cd "${BUILD_DIR}"
+cd "${BUILD_DIR}"
 
-cmake "${SOURCE_DIR}" -DCMAKE_BUILD_TYPE=Release -G "Unix Makefiles" -A x64 "${BUILD_DIR}"
+cmake "${SOURCE_DIR}" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN}" -DDRACO_WASM=ON
 make 
 echo "-- build complete --"
-cd ..
+
