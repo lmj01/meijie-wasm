4e090c5371ae5236324bc32b4432d65e7206fa12
lmj01 <lmjie_good@163.com>
2024-12-27 10:25:58 +0800

    add test

--- cpp/linuxDeformationBuild.sh
+++ cpp/linuxDeformationBuild.sh
@@ -3,6 +3,10 @@ SOURCE_DIR="$PWD/deformation"
 TOOLCHAIN="$PWD/build/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
 BUILD_DIR="$PWD/bdeformation"
 
+TARGET_DIR="$PWD/../packages/mjdeformation/public/libs/meijie"
+
+FNAMES=("deformation-wasm.js" "deformation-wasm.wasm")
+
 export EMSCRIPTEN="$PWD/build/emsdk/upstream/emscripten"
 
 if [ ! -d "${BUILD_DIR}" ]; then
@@ -16,4 +20,15 @@ cd "${BUILD_DIR}"
 cmake "${SOURCE_DIR}" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN}" -DDRACO_WASM=ON
 make 
 echo "-- build complete --"
+
+if [ ! -d "${TARGET_DIR}" ]; then
+	mkdir -p "${TARGET_DIR}"
+else
+	echo "folder exist"
+fi
+
+for name in "${FNAMES[@]}"
+do
+	cp "${BUILD_DIR}/$name" "${TARGET_DIR}/$name"
+done
 
--- cpp/linuxDracoBuild.sh
+++ cpp/linuxDracoBuild.sh
@@ -3,6 +3,9 @@ SOURCE_DIR="$PWD/build/draco"
 TOOLCHAIN="$PWD/build/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake"
 BUILD_DIR="$PWD/builddraco"
 
+TARGET_DIR="$PWD/../packages/mjdraco/public/libs/meijie"
+FNAMES=("draco_encoder.js" "draco_encoder.wasm" "draco_decoder.js" "draco_decoder.wasm")
+
 export EMSCRIPTEN="$PWD/build/emsdk/upstream/emscripten"
 
 if [ ! -d "${BUILD_DIR}" ]; then
@@ -11,9 +14,20 @@ if [ ! -d "${BUILD_DIR}" ]; then
 else 
     echo "fold exist: $BUILD_DIR"
 fi
+
 cd "${BUILD_DIR}"
 
 cmake "${SOURCE_DIR}" -DCMAKE_TOOLCHAIN_FILE="${TOOLCHAIN}" -DDRACO_WASM=ON
 make 
 echo "-- build complete --"
 
+if [ ! -d "${TARGET_DIR}" ]; then
+	mkdir -p "${TARGET_DIR}"
+else
+	echo "target folder exist"
+fi
+
+for name in "${FNAMES[@]}"
+do 
+    cp "${BUILD_DIR}/$name" "${TARGET_DIR}/$name"
+done
