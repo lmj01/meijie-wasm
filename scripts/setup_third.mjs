e3a6a8b38009143c613bf0a4372fb26c3ea9f7b7
lmj01 <lmjie_good@163.com>
2025-01-20 07:06:55 +0800

    add more third

---
+++ scripts/setup_third.mjs
@@ -0,0 +1,88 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { execAsync } from './common.mjs';
+
+const THIRD_ROOT = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../third/')
+
+const UE5_DIR_NAME = 'ue5';
+const UE5_DIR = path.resolve(THIRD_ROOT, UE5_DIR_NAME);
+
+const MESHOPTIMIZER_DIR_NAME = 'meshoptimizer';
+const MESHOPTIMIZER_DIR = path.resolve(THIRD_ROOT, MESHOPTIMIZER_DIR_NAME);
+
+const MANIFOLD_DIR_NAME = 'manifold';
+const MANIFOLD_DIR = path.resolve(THIRD_ROOT, MANIFOLD_DIR_NAME);
+
+async function updateSubmoduleDraco() {
+    // await execAsync(`git submodule init`);
+    console.log(DRACO_DIR)
+    await execAsync(`cd ${DRACO_DIR} && git submodule sync`);
+    await execAsync(`cd ${DRACO_DIR} && git submodule update --init -f --recursive`);
+}
+
+const libs = [
+    {
+        name: 'meshoptimizer',
+        url: 'https://github.com/zeux/meshoptimizer.git',
+        tag: 'v0.22',
+        dir: MESHOPTIMIZER_DIR,
+        action: [],
+        command: undefined
+    },
+    {
+        name: 'ue5',
+        url: 'git@github.com:EpicGames/UnrealEngine.git',
+        tag: '5.5.1-release',
+        dir: UE5_DIR,
+        action: [],
+        command: undefined
+    },
+    {
+        name: 'manifold',
+        url: 'https://github.com/elalish/manifold.git',
+        tag: 'v3.0.1',
+        dir: MANIFOLD_DIR,
+        action: [],
+        command: undefined
+    }
+]
+
+async function setupLibs() {
+    for (const lib of libs) {
+        if (!fs.existsSync(lib.dir)) {
+            console.log(`Cloning ${lib.name}...`);
+            await execAsync(`git clone --depth=1 -b ${lib.tag} ${lib.url} ${lib.dir}`);
+
+            if (!fs.existsSync(lib.dir)) {
+                console.error(`Failed to clone ${lib.name}`);
+                process.exit(1);
+            }
+        }
+
+        console.log(`Seting up ${lib.name}...`);
+        if (lib.command) {
+            await execAsync(lib.command);
+        }
+
+        for (const action of lib.action) {
+            await action();
+        }
+    }
+}
+
+function main() {
+    if (!fs.existsSync(THIRD_ROOT)) {
+        fs.mkdirSync(THIRD_ROOT);
+    }
+
+    setupLibs()
+        .then(e => {
+            console.log('Setup complete');
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+main();
